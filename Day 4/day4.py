4591bb1eba229f3355ef55695ece838bc7622e1c
Felipe Correa <fcu423@gmail.com>
2022-12-04 01:16:00 -0500

    Fix comment

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -19,9 +19,9 @@ for line in lines:
         rangesAnyOverlap += 1
     elif int(range2[1]) >= int(range1[0]) and int(range2[1]) <= int(range1[1]): #range 2 ends inside range 1
         rangesAnyOverlap += 1
-    elif int(range1[0]) >= int(range2[0]) and int(range1[0]) <= int(range2[1]): #range 1 starts inside range 2 or vice versa
+    elif int(range1[0]) >= int(range2[0]) and int(range1[0]) <= int(range2[1]): #range 1 starts inside range 2
         rangesAnyOverlap += 1
-    elif int(range2[0]) >= int(range1[0]) and int(range2[0]) <= int(range1[1]): #range 2 starts inside range 1 or vice versa
+    elif int(range2[0]) >= int(range1[0]) and int(range2[0]) <= int(range1[1]): #range 2 starts inside range 1
         rangesAnyOverlap += 1
         
 print(rangesDuplicatedCount)
